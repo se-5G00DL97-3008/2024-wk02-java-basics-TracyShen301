e66770b3933e2819653e597df8508a5928637d45
Lingyan Shen <148793722+TracyShen301@users.noreply.github.com>
2024-01-16 20:59:27 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -15,7 +15,7 @@ class E02 {
         System.out.println(num1 + " + " + num2 + " = " + sum);
         
         System.out.println(num1 + " - " + num2 + " = " + difference);
-        System.out.println(num1 + " * " + num2 + " = " + product);
+        System.out.println(num1 + " x " + num2 + " = " + product);
         System.out.println(num1 + " / " + num2 + " = " + quotient);
 
     }
