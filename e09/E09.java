d1289d01b2068cc9545cac071c4c7e8ebf5e1925
Lingyan Shen <148793722+TracyShen301@users.noreply.github.com>
2024-01-16 20:19:09 +0200

    Update E09.java

--- e09/E09.java
+++ e09/E09.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 class E09 {
     public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
 
+        int total = 0;
+        int time = 0;
+        while (true) {
+            System.out.println("Give a test score (-1 to quit):");
+            int score = scanner.nextInt();
+            if (score == -1) {
+                break;
+            }
+            total += score;
+            time++;
+            double average = (double)total/time;
+            System.out.println("Average:"+ average);
     }
+}
 }
