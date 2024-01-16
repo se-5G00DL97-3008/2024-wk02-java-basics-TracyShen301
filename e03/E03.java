dca57320d86beb98fd57c93e4a613a388937b86c
Lingyan Shen <148793722+TracyShen301@users.noreply.github.com>
2024-01-16 19:52:48 +0200

    Update E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -4,10 +4,10 @@ class E03 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number:");
         int number = scanner.nextDouble();
-        if(number > 0) {
+        if (number > 0) {
         System.out.println("Positive number");
         }
-        if(number < 0) {
+        if (number < 0) {
         System.out.println("Negative number");
         }
     }
