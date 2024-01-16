3a7fa00d054edfadaf3ac077776cf7e4ef40e907
Lingyan Shen <148793722+TracyShen301@users.noreply.github.com>
2024-01-16 19:58:10 +0200

    Update E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 class E03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number:");
-        int number = scanner.nextDouble();
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
         if (number > 0) {
         System.out.println("Positive number");
         }
-        if (number < 0) {
+        else if (number < 0) {
         System.out.println("Negative number");
         }
     }
