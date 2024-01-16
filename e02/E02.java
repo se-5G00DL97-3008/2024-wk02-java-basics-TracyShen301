bae5cd3ec77717eb61a806acbe844265afae1e66
Lingyan Shen <148793722+TracyShen301@users.noreply.github.com>
2024-01-16 20:36:48 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -3,14 +3,14 @@ class E02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Give a number:");
-        double num1 = input.nextDouble();
+        int num1 = input.nextInt();
         System.out.println("Give another number: ");
-        double num2 = input.nextDouble();
+        int num2 = input.nextInt();
 
-        double sum = num1 + num2;
-        double product = num1 * num2;
-        double difference = num1 - num2;
-        double quotient = num1 / num2;
+        int sum = num1 + num2;
+        int product = num1 * num2;
+        int difference = num1 - num2;
+        int quotient = num1 / num2;
 
         System.out.println(num1 + " + " + num2 + " = " + sum);
         System.out.println(num1 + " * " + num2 + " = " + product);
