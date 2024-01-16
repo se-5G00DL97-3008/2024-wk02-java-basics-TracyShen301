b0cfc66712fd7f64c52d234fd6f31bb8719a1e7a
Lingyan Shen <148793722+TracyShen301@users.noreply.github.com>
2024-01-16 21:23:34 +0200

    Update E07.java

--- e07/E07.java
+++ e07/E07.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class E07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Which multiplicand multiplication table to show?");
+        System.out.println("Which multiplication table to show?");
         int num1 = scanner.nextInt();
         System.out.println("Until which multiplier should the table show?");
         int num2 = scanner.nextInt();
