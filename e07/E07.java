60bf4ac2ce2f32564b49a0f4e9afe67927ce0758
Lingyan Shen <148793722+TracyShen301@users.noreply.github.com>
2024-01-16 20:13:30 +0200

    Update E07.java

--- e07/E07.java
+++ e07/E07.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 class E07 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which multiplicand multiplication table to show?");
+        int num1 = input.nextInt();
+        System.out.print("Until which multiplier?");
+        int num2 = scanner.nextInt();
+        for (int i = 0; i <= num2; i++) {
+        int multiplication = i * num1;
+        System.out.println(i + " x " + num1 + " = " + multiplication);
+        }
     }
 }
